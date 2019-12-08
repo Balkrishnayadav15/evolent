02a8cc5aa463fa905b25b10130c63edc5a2c8169
Balkrishna Yadav <balyadav@cisco.com>
2019-12-08 20:22:21 +0530

    setup springboot service and ui layer

---
+++ evolent-user-rest-service/src/main/java/com/evolenthealth/evolentuserrestservice/EvolentUserRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evolenthealth.evolentuserrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolentUserRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolentUserRestServiceApplication.class, args);
+	}
+
+}
---
+++ evolent-user-rest-service/src/test/java/com/evolenthealth/evolentuserrestservice/EvolentUserRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolenthealth.evolentuserrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolentUserRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
